8851d280a1206298a31d30eae29c2ebd7bf93d3c
Bleyle823 <bennybleylle09@gmail.com>
2025-08-26 22:26:46 +0300

    Update turbopack root configuration in next.config.js to use process.cwd() instead of __dirname

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import("next").NextConfig} */
-const config = {
-  experimental: {
-    turbopack: {
-      // Explicitly set root to this workspace to silence inferred root warning
-      root: __dirname,
-    },
-  },
-};
+const config = {};
 
 export default config;
