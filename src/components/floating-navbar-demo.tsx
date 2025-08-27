8682b065df562b18a5c525127c9fb26438503ebc
Bleyle823 <bennybleylle09@gmail.com>
2025-08-28 00:00:35 +0300

    Refactor FloatingDock component to enhance performance by optimizing image handling with Next.js Image component, improving loading times and visual quality.

---
+++ src/components/floating-navbar-demo.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { IconHome, IconExchange, IconTerminal2, IconNewSection, IconBrandGithub, IconBrandX } from "@tabler/icons-react";
+
+export default function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Bridge",
+      link: "/bridge",
+      icon: <IconExchange className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Products",
+      link: "/products",
+      icon: <IconTerminal2 className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Components",
+      link: "/components",
+      icon: <IconNewSection className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "GitHub",
+      link: "https://github.com/lifinance",
+      icon: <IconBrandGithub className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/lifiprotocol",
+      icon: <IconBrandX className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+  ];
+
+  return (
+    <div className="relative w-full">
+      <FloatingNav navItems={navItems} />
+      <DummyContent />
+    </div>
+  );
+}
+
+const DummyContent = () => {
+  return (
+    <div className="grid grid-cols-1 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md">
+      <p className="dark:text-white text-neutral-600 text-center text-4xl mt-40 font-bold">
+        Scroll back up to reveal Navbar
+      </p>
+      <div className="inset-0 absolute bg-grid-black/[0.1] dark:bg-grid-white/[0.2]" />
+    </div>
+  );
+};
